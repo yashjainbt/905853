f24a33d098022ff891fe2c8bb294215ccc7aaebd
Yash Jain <yashjain9058@gmail.com>
2026-03-16 14:10:40 +0530

    Update hero.ts

--- content/hero.ts
+++ content/hero.ts
@@ -1,7 +1,6 @@
 export const heroContent = {
   name: 'Yash Jain',
-  primaryLine: 'Biotechnology Researcher | 
-    Medical Device Quality & Regulatory Specialist',
+  primaryLine: 'Biotechnology Researcher | <br> Medical Device Quality & Regulatory Specialist',
   supportingLine:
     'Integrating experimental oncology with regulatory science and translational systems.',
   microStatement:
