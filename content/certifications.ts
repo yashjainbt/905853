8b80c3b612b9ed673d25cb9d1a1e95ee19185863
Yash Jain <yashjain9058@gmail.com>
2026-03-19 22:21:49 +0530

    Update certifications.ts

--- content/certifications.ts
+++ content/certifications.ts
@@ -1,6 +1,6 @@
 export const certifications = [
-  'Graduate Aptitude Test in Engineering (GATE) 2026 — Score: 521, All India Rank: 699',
-  'Graduate Aptitude Test in Engineering (GATE) 2025 — Score: 356, All India Rank: 2901',
+  'Graduate Aptitude Test in Engineering-Biotechnology (GATE-BT) 2026 — Score: 521, All India Rank: 699',
+  'Graduate Aptitude Test in Engineering-Biotechnology (GATE-BT) 2025 — Score: 356, All India Rank: 2901',
   'Scientific Writing Training — MIET, May–July 2022',
   'Production of Bioethanol from Sugarcane Bagasse — MIET, Nov–Dec 2022'
 ];
